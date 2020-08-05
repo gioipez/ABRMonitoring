d18fc1b97d4721a89b473814c5e806bea729106e
GioLopez <giovanni.lopez@cwc.com>
2020-08-05 12:20:26 -0500

    More comments added to functions, and more functions implemented

--- library/hls_manifest.py
+++ library/hls_manifest.py
@@ -15,7 +15,7 @@ class HLSManifest():
     Attributes:
         * asset_url
         * manifest_text
-        * submanifest_url
+        * submanifest_text_url
         * submanifest_text
 
     Parameters:
@@ -29,9 +29,10 @@ class HLSManifest():
     submanifests_text = ""
     audio_info_list = ""
     audio_json = {}
-    video_info_list = ""
+    video_info_list = []
     video_json = {}
-    video_sub_manifest_url = {}
+    sub_manifest_url = {}
+    video_files = {}
 
     def __init__(self, asset_url):
         super(HLSManifest, self).__init__()
@@ -40,6 +41,9 @@ class HLSManifest():
     def get_manifest_text(self):
         """
         Get manifest response base on given URL
+        Output:
+            - self.manifest_text = [str] HLS manifest information
+        NOTE: self.asset_url should not be empty
         """
         manifest_text = basic.get_request(self.asset_url)
         if manifest_text:
@@ -47,25 +51,43 @@ class HLSManifest():
 
     def parse_manifest(self):
         """
-        Parse Manifest to JSON
+        Parse Manifest to JSON, including video and audio info
         """
         self.parse_audio()
         self.parse_video()
 
     # Build submanifest URL ########################
 
-    def build_submanifest_url_for_video(self, base_url, profile_number):
+    def build_submanifest_url(self, base_url, profile_number):
         """
         Build the submanifest URL of the given asset
         Parameters:
-            - base_url: is the CDN/Origin URL that will helps
+            - base_url: [str] is the CDN/Origin URL that will helps
                 to create the URL
-            - profile_number: video profile that will be retrieve
+            - profile_number: [int] video profile that will be retrieve
+        Output:
+            - self.submanifest_url [dict] filled with submanifest urls
         NOTE: self.manifest_text should not be empty
         """
         video_url = self.video_json[f"video_{profile_number}"]["URI"]
         temp_va = f"{base_url}{video_url}"
-        self.video_sub_manifest_url[f"sub_manifest_{profile_number}"] = temp_va
+        self.sub_manifest_url[f"sub_manifest_{profile_number}"] = temp_va
+
+    def extract_video_files_from_submanifest(self, sub_manifest_url):
+        """
+        Given the selected sub_manifest URL, it extract
+        Parameters:
+            - sub_manifest_url [str] selected sub manifest url
+        Output:
+            - self.video_files [dict] of all videos in the submanifest
+        """
+        submanifest_text = basic.get_request(sub_manifest_url)
+        video_chunk = 0
+        if submanifest_text:
+            for item in submanifest_text.text.split("\n"):
+                if "#EXT" not in item and item != "":
+                    self.video_files[f"{video_chunk}"] = item
+                    video_chunk += 1
 
     # Build submanifest URL END ####################
 
@@ -73,7 +95,11 @@ class HLSManifest():
 
     def parse_video(self):
         """
-        Parse all video info
+        Parse all video info, callin methods:
+            - get_video_information
+            - parse_video_to_json
+        As a result this will handle all the information that manifest
+        have about video
         """
         self.get_video_information()
         self.parse_video_to_json()
@@ -81,6 +107,9 @@ class HLSManifest():
     def get_video_information(self):
         """
         Get video(s) URL from manifest as list
+        Output:
+            - self.video_info_list [list] with all video profile info
+        NOTE: self.manifest_text should have manifest information
         """
         video_profiles = []
         for items in enumerate(self.manifest_text.split("\n")):
@@ -94,6 +123,9 @@ class HLSManifest():
     def parse_video_to_json(self):
         """
         Parse information from list to json
+        Output:
+            - self.video_json [dict] with all video information
+        NOTE: self.video_info_list should have information
         """
         vid_num = 0
         video_json = {}
@@ -118,14 +150,34 @@ class HLSManifest():
 
     def parse_audio(self):
         """
-        Parse all audio info
+        Parse all audio info, calling methods:
+            - get_audio_information
+            - parse_audio_to_json
+        As a result this will handle all the information that manifest
+        have about audio
         """
         self.get_audio_information()
         self.parse_audio_to_json()
 
+    def get_audio_information(self):
+        """
+        Get audio(s) URL from manifest as list
+        Output:
+            - self.audio_info_list [list] with all audio profile info
+        NOTE: self.manifest_text should have manifest information
+        """
+        audio_list = []
+        for items in self.manifest_text.split("\n"):
+            if "TYPE=AUDIO" in items:
+                audio_list.append(items)
+        self.audio_info_list = audio_list
+
     def parse_audio_to_json(self):
         """
         Parse information from list to json
+        Output:
+            - self.audio_json [dict] with all audio information
+        NOTE: self.audio_info_list should have information
         """
         audio_json = {}
         au_t = []
@@ -140,18 +192,7 @@ class HLSManifest():
             audio_num += 1
         self.audio_json = audio_json
 
-    def get_audio_information(self):
-        """
-        Get Audio(s) URL from manifest as list
-        """
-        audio_list = []
-        for items in self.manifest_text.split("\n"):
-            if "TYPE=AUDIO" in items:
-                audio_list.append(items)
-        self.audio_info_list = audio_list
-
     # Audio parse section END ######################
-
 
 if __name__ == "__main__":
     # HLS URL
@@ -167,8 +208,14 @@ if __name__ == "__main__":
 
     # Submaniefst build
     for profiles in range(5):
-        hls_object.build_submanifest_url_for_video(BASE_URL, profiles)
+        hls_object.build_submanifest_url(BASE_URL, profiles)
+
+    # Build video URL
+    hls_object.extract_video_files_from_submanifest(
+        hls_object.sub_manifest_url["sub_manifest_0"])
+
     print(json.dumps({
         "audio": hls_object.audio_json,
         "video": hls_object.video_json,
-        "sub_manifest": hls_object.video_sub_manifest_url}))
+        "video_chunks": hls_object.video_files,
+        "sub_manifest": hls_object.sub_manifest_url}))
